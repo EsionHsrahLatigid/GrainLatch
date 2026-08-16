85a816f160f10a2015f0a4eb1590444978668185
2bit <github@2bit.jp>
2026-08-17 03:57:51 +0900

    Align public parameter identifiers

--- Source/ParameterIDs.h
+++ Source/ParameterIDs.h
@@ -4,12 +4,12 @@ namespace grainlatch::parameters
 {
 inline constexpr auto grainMs = "grainMs";
 inline constexpr auto density = "density";
-inline constexpr auto pitch = "pitch";
-inline constexpr auto position = "position";
-inline constexpr auto dispersion = "dispersion";
-inline constexpr auto latch = "latch";
+inline constexpr auto jitter = "jitter";
+inline constexpr auto reverse = "reverse";
+inline constexpr auto stutter = "stutter";
+inline constexpr auto retrigger = "retrigger";
 inline constexpr auto freeze = "freeze";
-inline constexpr auto feedback = "feedback";
+inline constexpr auto damage = "damage";
 inline constexpr auto mix = "mix";
 inline constexpr auto output = "output";
 } // namespace grainlatch::parameters
